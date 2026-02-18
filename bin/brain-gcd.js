89eac866179543999c3f3ad867d32ecec97da638
pkryazhev <kryazhevpavel94@gmail.com>
2026-02-18 18:11:06 +0300

    step 7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import runBrainGcd from "../src/games/brain-gcd.js";
+
+runBrainGcd();
---
+++ src/games/brain-gcd.js
@@ -0,0 +1,33 @@
+import { generateNumber, getName, checkAnswer, getAnswer, printCongratulations } from '../index.js';
+
+const runBrainGcd = () => {
+  const name = getName();
+  console.log('Find the greatest common divisor of given numbers.');
+  for (let i = 0; i < 3; i += 1) {
+    const number1 = generateNumber();
+    const number2 = generateNumber();
+    const question = `${number1} ${number2}`;
+    const answer = getAnswer(question);
+    const correctAnswer = getCorrectAnswer(number1, number2);
+    if (!checkAnswer(answer, correctAnswer, name)) {
+      return;
+    }
+  }
+  printCongratulations(name);
+}
+
+const getCorrectAnswer = (number1, number2) => {
+    let num1 = number1;
+    let num2 = number2
+    if (num2 === 0) {
+        return num1;
+    }
+    while (num2 !== 0) {
+        const temp = num1 % num2;
+        num1 = num2;
+        num2 = temp;
+    }
+    return num1.toString();
+}
+
+export default runBrainGcd;
